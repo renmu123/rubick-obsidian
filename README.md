b660cd4bca179d148b807b082e272e75a3fb8d89
renmu123 <1101022351@qq.com>
2022-02-17 15:30:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# rubick-vue-plugin-template
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run watch
+```
+
+### install the plugin
+
+1. `npm run watch`
+2. cd to the dist folder, then you can run `npm link`
+3. install the plugin in rubick
+4. restart rubick
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    hello Rubick
+    <button id="showNotification">通知</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  // components: {
+  //   HelloWorld,
+  // },
+  mounted() {
+    document
+      .getElementById("showNotification")
+      .addEventListener("click", () => {
+        window.showNotification();
+      });
+  },
+};
+</script>
